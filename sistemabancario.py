5972213563782fdabe1129e5d12bb6593d8ebeb7
werickgabriel <gabriel_marinhoveras@hotmail.com>
2025-09-09 15:04:03 -0300

    adicionado metodos de criar usuario e conta corrente

--- sistemabancario.py
+++ sistemabancario.py
@@ -1,6 +1,14 @@
+
+import re
+from datetime import datetime, timezone
+
+
 saldo = 0.00
-limite_saque = 3
+limite_transacao = 10
 extrato = []
+usuarios = []
+contas_correntes = []
+
 
 menu = (
     """=========================================== 
@@ -9,7 +17,9 @@ menu = (
         [1] Depósito
         [2] Saque
         [3] Extrato
-        [4] Finalizar sessão
+        [4] Cadastrar usuário
+        [5] Criar conta corrente
+        [6] Finalizar sessão
        =========================================== 
     """
 )
@@ -19,7 +29,7 @@ def deposito(valor):
     global extrato
 
     saldo += valor
-    extrato.append(f"Depósito de R${valor: .2f}")
+    extrato.append(f"Depósito de R${valor: .2f}. {datetime.strftime(datetime.now(timezone.utc), '%d/%m/%Y %H:%M')}")
     
 
 def saque(valor):
@@ -29,6 +39,72 @@ def saque(valor):
     saldo -= valor
     extrato.append(f"Saque de R${valor: .2f}")
     
+def transacao():
+    global limite_transacao
+
+    if (limite_transacao == 0):
+        print("Você ultrapassou seu limite de transações")
+        return 0
+
+    else:
+        limite_transacao -= 1
+        return 1    
+
+def extrato():
+    print("===============EXTRATO================")
+    print("\n".join(extrato) + '\n')
+    print(f"Saldo: {saldo: .2f}")
+
+
+def criar_usuario():
+    def formatar_cpf():
+        cpf = input('Digite o seu cpf: \n')
+        cpf = re.sub(r'\D', '', cpf)
+
+        if (len(cpf) == 11):
+            return cpf
+        else:
+            print('cpf inválido, tente novamente.')
+            return formatar_cpf()
+
+    cpf = formatar_cpf()
+
+    if any(user['cpf'] == cpf for user in usuarios):
+        print('Já existe um usuário com este cpf')
+
+    else:
+        nome = input('Digite seu nome e sobrenome: \n').lower()
+        nascimento = input('Digite sua data de nascimento: dd/mm/YYYY \n')
+        nascimento = datetime.strptime(nascimento, '%d/%m/%Y')
+        endereco = input('Digite seu endereço com: Logradouro, bairro - cidade/sigla do estado: \n')
+
+        usuario = {
+            'nome' : nome,
+            'nascimento' : nascimento,
+            'cpf' : cpf,
+            'endereco' : endereco
+        }
+
+        usuarios.append(usuario)
+        print("Usuário cadastrado com sucesso!")
+    
+def criar_conta_corrente():
+    nome_usuario = input("Digite o nome e sobrenome do usuário pra vincular esta conta:\n").lower()
+    if any(user['nome'] == nome_usuario for user in usuarios ):
+        print('Conta vinculada com sucesso')
+        numero_conta = len(contas_correntes) + 1
+        agencia = input('Digite o número de sua agência:\n')
+        conta = {
+            'nome': nome_usuario,
+            'conta': numero_conta,
+            'agencia': agencia
+        }
+
+        contas_correntes.append(conta)
+        print('Conta corrente criada com sucesso!')
+
+    else:
+        print('Nao existe um usuario cadastrado com esse nome.')
 
 
 while (True):
@@ -36,27 +112,26 @@ while (True):
 
     opcao_usuario = int(input())
 
-    if (opcao_usuario == 4):
+    if (opcao_usuario == 6):
         print("=============SESSÃO FINALIZADA==============")
         break
 
     if (opcao_usuario == 1):
-        print("Digite o valor que deseja depositar")
-        valor = float(input())
+        if (transacao() > 0):
+            print("Digite o valor que deseja depositar")
+            valor = float(input())
 
-        if (valor <= 0):
-            print("Valor inválido, operação encerrada.")
+            if (valor <= 0):
+                print("Valor inválido, operação encerrada.")
         
-        else:
-            deposito(valor)
-            print("Operação concluída")
+            else:
+                deposito(valor)
+                print("Operação concluída")
            
 
     if (opcao_usuario == 2):
-        if (limite_saque > 0): 
-            print(f"Digite o valor que deseja sacar. Você tem {limite_saque} saques restantes.")
-            valor = float(input())
-
+        
+        if (transacao() > 0):    
             if (valor > 500):
                print("Saques maiores que R$500,00 não são permitidos, tente um valor menor.")
             
@@ -65,14 +140,15 @@ while (True):
             
             else:
                saque(valor)
-               limite_saque -= 1
-               print("Operação concluída")
-                    
-        else:
-            print("Você alcançou seu limite de saque do dia, tente novamente amanhâ.")
+               limite_transacao -= 1
+               print("Operação concluída")        
         
 
     if (opcao_usuario == 3):
-        print("===============EXTRATO================")
-        print("\n".join(extrato) + '\n')
-        print(f"Saldo: {saldo: .2f}")
+        extrato()
+
+    if (opcao_usuario == 4):
+        criar_usuario()    
+
+    if (opcao_usuario == 5):
+        criar_conta_corrente()
